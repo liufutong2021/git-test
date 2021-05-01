8c5a6226cbd2521499df5e9a6ae1e500304de145
liuyi <liuyi@163.com>
2021-05-01 08:56:57 +0800

    first lft commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 刘富通
+ * @create 2021-05-01 8:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
